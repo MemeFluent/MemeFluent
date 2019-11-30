6c6991f984f02f604556cabd893f125b48bf5110
Lily Tran <litran@pdx.edu>
2019-11-30 12:32:39 -0800

    Add modules

---
+++ App.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const dateTime = require("simple-datetime-formater");
+const bodyParser = require("body-parser");
+const chatRouter = require("./route/chatroute");
+const loginRouter = require("./route/loginRoute");
+
+const http = require("http").Server(app);
+
+const io = require("socket.io");
+
+const port = 5000;
