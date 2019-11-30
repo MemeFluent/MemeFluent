7d8ad9fae3f9a104353b7b709ee9c2b2b874a36a
Lily Tran <litran@pdx.edu>
2019-11-30 12:45:56 -0800

    Use middleware and add routes

--- App.js
+++ App.js
@@ -10,3 +10,19 @@ const http = require("http").Server(app);
 const io = require("socket.io");
 
 const port = 5000;
+
+app.use(bodyParser.json());
+
+app.use("/chats", chatRouter);
+app.use("/login", loginRouter);
+
+app.use(express.static(__dirname + "/public"));
+
+socket = io(http);
+
+const Chat = require("./models/Chat");
+const connect = require("./dbconnect");
+
+http.listen(port, () => {
+  console.log("Running on Port: " + port);
+});
